63be0be98526c360a0b498e77fa658cb0f13be4f
rnava3010 <rnava3010@gmail.com>
2026-01-12 21:49:00 -0600

    modify buttons 2

--- manager/vite.config.js
+++ manager/vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/manager/',
+  base: '/manager/', 
+  build: {
+    chunkSizeWarningLimit: 1600, 
+    
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor': ['react', 'react-dom', 'react-router-dom', 'axios'],
+          'icons': ['lucide-react'],
+        }
+      }
+    }
+  }
 })
