68136ed49489eb0931495cec664a90c3a92bbc41
rnava3010 <rnava3010@gmail.com>
2026-01-12 22:42:48 -0600

    modify conf 4

---
+++ manager/src/pages/Configuracion.jsx
@@ -0,0 +1,3 @@
+export default function Configuracion() {
+  return <h1 className="text-white text-2xl">Configuracion</h1>;
+}
---
+++ manager/src/pages/Usuarios.jsx
@@ -0,0 +1,3 @@
+export default function Usuarios() {
+  return <h1 className="text-white text-2xl">Usuarios</h1>;
+}
